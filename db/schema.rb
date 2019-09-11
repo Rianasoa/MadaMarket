9fa7cf09e89f08dacb7d0ab465a0ed872e72041c
Rianasoa <rrianasoaonimalala@gmail.com>
2019-09-11 14:31:52 +0300

    keyMailer

--- db/schema.rb
+++ db/schema.rb
@@ -114,6 +114,8 @@ ActiveRecord::Schema.define(version: 2019_09_11_063650) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_admin", default: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

07b4436b2d709f9e0c8ed66a8edbddb229b2f829
VerizaWalter <verizawalter@gmail.com>
2019-09-11 10:22:00 +0300

    creating development  UserMailer, Active_storage, City Table

---
+++ app/models/city.rb
@@ -0,0 +1,3 @@
+class City < ApplicationRecor
+	has_many :users
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   	has_one :cart
+  	belongs_to :city
+  	has_one_attached :avatar
   	
   	has_many :commands, foreign_key: 'customer_id'
   	has_many :products, foreign_key: 'provider_id'
+
+
+  after_create :welcome_send
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end

1c4ead2b8b69dd46a1c7a824db93bef92919126e
Rianasoa <rrianasoaonimalala@gmail.com>
2019-09-17 11:09:45 +0300

    template

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,13 @@ class CategoriesController < ApplicationController
 
   def new
   end
+  def show
+    @products = Product.all
+    if user_signed_in? && current_user.is_admin == true
+      redirect_to admin_products_path
+    end 
+    @category = Category.find(params[:id])
+  end
 
   def index
   end
